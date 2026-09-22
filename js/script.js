a70c4fa76fc383b2fa88402bff2e9f1706901ea7
DOT190906 <diegoortiz190906@gmail.com>
2026-09-22 09:40:17 -0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const name = "Rosa Mesa";
+
+let age = 69;
+
+let proyectoFavorito = "Mi cosa web";
+let NOMBRE = "Benito Carlo";
+
+function mostrarProyecto(nombreProyecto) {
+    console.log(nombreProyecto);
+}
+
+mostrarProyecto(proyectoFavorito);
+mostrarProyecto("");
+
+console.log(NOMBRE.toUpperCase());
