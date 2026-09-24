dce89b4cb0f6e519f204a3d0bebacb4672e77eb7
DOT190906 <diegoortiz190906@gmail.com>
2026-09-24 09:50:46 -0600

    commit

--- js/script.js
+++ js/script.js
@@ -1,15 +1,17 @@
-const name = "Rosa Mesa";
+let edad = parseInt(prompt("Por favor, ingresa tu edad:"));
 
-let age = 69;
+switch (true) {
+    case (edad <= 17):
+        alert("Eres un niño.");
+        break;
+    case (edad === 18):
+        alert("Eres un adolescente.");
+        break;
+    case (edad >= 19):
+        alert("Eres un adulto.");   
+    break;
+    default:
+        alert("Edad no válida.");
+        break;
+}  
 
-let proyectoFavorito = "Mi cosa web";
-let NOMBRE = "Benito Carlo";
-
-function mostrarProyecto(nombreProyecto) {
-    console.log(nombreProyecto);
-}
-
-mostrarProyecto(proyectoFavorito);
-mostrarProyecto("");
-
-console.log(NOMBRE.toUpperCase());
